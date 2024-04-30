e150fd86019b4a330acd9f881a70c3c3214f22cb
wugawrld <168501851+wugawrld@users.noreply.github.com>
2024-04-30 09:54:33 -0700

    Update csv_to_vis.py

--- csv_to_vis.py
+++ csv_to_vis.py
@@ -4,50 +4,51 @@ import locale
 
 locale.setlocale(locale.LC_ALL, '')
 
-df = pd.read_csv('music.csv') # Creating variable to read CSV 
+# Creating variable to read the selected CSV.
+df = pd.read_csv('music.csv')
 
-df.dropna(subset=['Daily Streams'], inplace=True) # Appending additonal function to variable to drop rows (ignore essentially) that are missing integer values 
+# Appending additonal function to variable to drop rows (ignore essentially) that are missing integer values, as in this case we only want integer values. 
+df.dropna(subset=['Daily Streams'], inplace=True)
 
-# Ensuring the conversion of Overall & Daily Streams to numeric types
-
+# Ensuring the conversion of Overall & Daily Streams to numeric types.
 df['Overall Streams'] = df['Overall Streams'].str.replace(',', '').astype(int)
 df['Daily Streams'] = df['Daily Streams'].str.replace(',', '').astype(int)
 
-# Predetermining amount of songs selected
+# Predetermining amount of songs selected.
 df_subset = df.head(15)
-
+# Creating variables that will correspond with the selected number of songs and their respective labels. 
 song_title = df_subset['Song Title']
 overall_streams = df_subset['Overall Streams']
 daily_streams = df_subset['Daily Streams']
 
-# Sort the overall streams data in descending order and reset index
+# Sort the overall streams data in descending order and reset index.
 sorted_overall_streams = overall_streams.sort_values(ascending=False).reset_index(drop=True)
+# Setting custom size of figure.
+plt.figure(figsize=(15, 8))
 
-plt.figure(figsize=(15, 8))  # Setting custom size of figure
-
-# Plot the scatter plot using sorted indices
+# Plot the scatter plot using sorted indices.
 plt.scatter(range(len(sorted_overall_streams)), sorted_overall_streams, color='b', label='Overall Streams')
 plt.scatter(range(len(sorted_overall_streams)), daily_streams, color='r', label='Daily Streams')
-
+# Adjusting the aesthetic of the labels (font size, type, etc.) 
 plt.xlabel('Songs', fontweight='bold', fontsize=10)
 plt.ylabel('Streams', fontweight='bold', fontsize=10)
 
-# Defining tick labels
+# Defining tick labels.
 sorted_song_title = song_title[overall_streams.argsort()][::-1]  # Reorder song titles based on sorted indices
 plt.xticks(range(len(sorted_overall_streams)), sorted_song_title, rotation=90, fontsize=5)
 
-# Assigning the numerical value to each plot [Annotation]
+# Assigning the numerical value to each plot (annotating the data).
 for i in range(len(song_title)):
     overall_y = sorted_overall_streams[i]
     daily_y = daily_streams.iloc[i]
 
-    # Determining the neighboring height of annotations
+    # Determining the neighboring height of annotations.
     avg_height = (overall_y + daily_y) / 2 
 
-    # Offsetting the annotations to prevent overlap
+    # Offsetting the annotations to prevent overlap.
     offset = 15 if daily_y > overall_y else -15
 
-    # Format the presentation of numbers (show place value commas)
+    # Format the presentation of numbers (show place value commas).
     overall_streams_label = locale.format_string("%d", overall_streams.iloc[i], grouping=True)
     daily_streams_label = locale.format_string("%d", daily_streams.iloc[i], grouping=True)
 
@@ -55,9 +56,9 @@ for i in range(len(song_title)):
     plt.text(i, daily_streams.iloc[i], daily_streams_label, fontsize=8, ha='center', va='bottom', color='red', fontweight='bold', bbox=dict(facecolor='white', edgecolor='none', pad=1))
 
 
-# Legend Adjustment
+# Legend Adjustment.
 plt.legend(loc='upper right', bbox_to_anchor=(1.25, 1))
-
-plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)  # Adjust the margins
+# Adjust the margins.
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
 
 plt.show()
